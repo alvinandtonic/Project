559d147e9b61dad9717e7d105ef0303052abdd13
alvinandtonic <112454403+alvinandtonic@users.noreply.github.com>
2022-12-28 22:54:29 -0500

    Add files via upload

---
+++ Lab4/LFSR_PRNG.v
@@ -0,0 +1,24 @@
+module LFSR_PRNG (input clk, rst, output reg [2:0] out);
+
+reg [5:0] D123456;
+	
+always @ (posedge clk, posedge rst) begin
+	
+	if(rst)
+		D123456<= 2'b01;
+			
+	else begin
+		D123456[1] <= D123456[0];
+		D123456[2] <= D123456[1];
+		D123456[3] <= D123456[2];
+		D123456[4] <= D123456[3];
+		D123456[5] <= D123456[4];
+		D123456[0] <= (D123456[5] ^ D123456[4]);
+	end			
+			
+	out[2] <= D123456[1];
+	out[1] <= D123456[3];
+	out[0] <= D123456[5];
+end
+		
+endmodule
